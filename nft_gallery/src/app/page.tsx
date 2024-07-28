157c543c0d5ad051fee3e7d17234fe77f0b1317e
Ponjinge <34633743+Ponjinge@users.noreply.github.com>
2024-07-28 02:08:42 +0200

    Update page.tsx
    Made page dynamic

--- nft_gallery/src/app/page.tsx
+++ nft_gallery/src/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { Key } from "react";
 import { db } from "~/server/db";
 
+// Make the page dynamic 
+export const dynamic = "force-dynamic";
 
 const mockUrls = [
   
