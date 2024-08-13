2bc5a4d975cc0f1e46b84afc8a811f0da0145b82
ascii-digital <gabrielsaunders44@gmail.com>
2024-08-13 23:19:59 +0100

    Loading from IPFS instead of database

--- nft_gallery/src/app/common/full-image-page.tsx
+++ nft_gallery/src/app/common/full-image-page.tsx
@@ -1,45 +1,45 @@
-import { getImage } from "~/server/queries";
 import { Card } from "~/components/ui/card";
+import { metadata } from "../layout";
 
-// we don't use params because it's not part of the route.
-export default async function FullPageImageView(props: { id: number }) {
-  const image = await getImage(props.id);
+// Function to convert IPFS URL to HTTP URL
+function convertIpfsUrl(ipfsUrl) {
+  return ipfsUrl.startsWith("ipfs://")
+    ? ipfsUrl.replace("ipfs://", "https://ipfs.io/ipfs/")
+    : ipfsUrl;
+}
+
+export default async function FullPageImageView(props: { id:  number }) {
+  let metadata;
+  
+  try {
+    const response = await fetch(`https://ipfs.io/ipfs/bafybeibc5sgo2plmjkq2tzmhrn54bk3crhnc23zd2msg4ea7a4pxrkgfna/${props.id}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch image data");
+    }
+    metadata = await response.json();
+    // Convert the image URL if it's an IPFS link
+    metadata.url = convertIpfsUrl(metadata.image);
+  } catch (err) {
+    console.error("Error fetching image data:", err);
+    return <div>Error loading image data.</div>;
+  }
+
   return (
     <div className="flex h-full w-screen min-w-0 items-center justify-center p-4">
       <Card className="min-w-6 max-w-6xl">
         <div className="flex flex-col justify-center md:flex-row">
           <img
-            src={image.url}
+            src={metadata.url}
             className="max-h-[75vh] max-w-full object-contain p-2"
-            alt={image.name}
+            alt={metadata.name}
           />
           <div className="flex h-full flex-shrink-0 flex-col border-l p-4">
-            
-            <div className="border-b p-2 text-center text-xl">{image.name}</div>
+            <div className="border-b p-2 text-center text-xl">{metadata.name}</div>
             <div className="p-2">
-              <div>Id: </div>
-              <div>{image.id}</div>
+              <div>Id:</div>
+              <div>{metadata.id}</div>
             </div>
-            <div className="p-2">
-              <div>Created On:</div>
-              <div>{new Date(image.createdAt).toLocaleDateString()}</div>
-            </div>
-            <div className="p-2">
-              <div>Created By:</div>
-              <div>{image.artist}</div>
-            </div>
-            <div className="p-2">
-              <div>Year:</div>
-              <div>{image.year}</div>
-            </div>
-            <div className="p-2">
-              <div>Description:</div>
-              <div>{image.description}</div>
-            </div>
-            <div className="p-2">
-              <div>Type:</div>
-              <div>{image.mediatype}</div>
-            </div>
+           
           </div>
         </div>
       </Card>
